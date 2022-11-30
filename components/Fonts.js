ffccda94cfcfc1f8ad7adcd318bd546d824f0c6e
theoyoth <theobantang1@gmail.com>
2022-12-01 00:23:42 +0700

    change font to poppins

--- components/Fonts.js
+++ components/Fonts.js
@@ -1,8 +1,6 @@
-import { Global } from "@emotion/react";
-
 const Fonts = () => (
   <style jsx global>{`
-    @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap");
     @import url("https://fonts.googleapis.com/css2?family=Pushster&display=swap");
   `}</style>
 );
