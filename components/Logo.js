d5586cfcad270bbd6bffa7cf88f74f3f770a2c9b
theoyoth <theobantang1@gmail.com>
2022-12-01 00:23:15 +0700

    change logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,21 +1,26 @@
-import { Container, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Box } from "@chakra-ui/react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Logo = () => {
   return (
-    <Container>
+    <Box>
       <Link href="/">
         <a>
           <Flex w="100px" alignItems="center" justifyContent="space-between">
-            <Image src="/image/logo.png" alt="logo" width="25" height="25" />
+            <Image
+              src="/image/logo-theo.png"
+              alt="logo"
+              width="25"
+              height="25"
+            />
             <Text fontFamily="Pushster, cursive" letterSpacing={1.1}>
               Theoyoth
             </Text>
           </Flex>
         </a>
       </Link>
-    </Container>
+    </Box>
   );
 };
 
